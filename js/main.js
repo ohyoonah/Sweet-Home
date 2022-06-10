20c53676fabbb8c71d26ec1444c8c3a560ea8025
ohyoonah <dhdbsdk3@naver.com>
2022-06-10 18:42:11 +0900

    이미지슬라이드 버튼

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
+var img2 = document.querySelectorAll(".image2 > img");
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
 var current = 0;
-image2();
-
-function image2() {
-  var img2 = document.querySelectorAll(".image2 > img");
-  for(let i = 0; i < img2.length; i++) {
-    img2[i].style.display = "none";
+image2(current);
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
+function image2(n) {
+  for(var i = 0; i < img2.length; i++) {
+    img2[i].style.display = "none"; 
   }
-  current++;
-  if(current > img2.length)
-    current = 1;
-  img2[current - 1].style.display = "block";
-  setTimeout(image2, 2000);
+  img2[n].style.display = "block"; 
+}
+function prevSlide() {
+  if(current > 0) current -= 1; 
+  else current = img2.length - 1;
+    image2(current);
+}
+function nextSlide() {
+  if(current < img2.length - 1) current += 1; 
+  else current = 0;
+    image2(current);
 }
