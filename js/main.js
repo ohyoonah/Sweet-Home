4d42708b33663b189faa57ef29cbfcdc50ffa68f
ohyoonah <dhdbsdk3@naver.com>
2022-06-10 18:16:55 +0900

    이미지 슬라이드

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var current = 0;
+image2();
+
+function image2() {
+  var img2 = document.querySelectorAll(".image2 > img");
+  for(let i = 0; i < img2.length; i++) {
+    img2[i].style.display = "none";
+  }
+  current++;
+  if(current > img2.length)
+    current = 1;
+  img2[current - 1].style.display = "block";
+  setTimeout(image2, 2000);
+}
